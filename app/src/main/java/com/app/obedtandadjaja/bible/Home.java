bb0e4b597993fa8b4b2bdf81204cb427969a0509
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2016-02-08 18:19:57 -0500

    updated Home activity to have comparison setting

--- app/src/main/java/com/app/obedtandadjaja/bible/Home.java
+++ app/src/main/java/com/app/obedtandadjaja/bible/Home.java
@@ -1,6 +1,7 @@
 package com.app.obedtandadjaja.bible;
 
 import android.app.Dialog;
+import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -20,31 +21,34 @@ import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import com.example.obedtandadjaja.bible.R;
 
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by obedtandadjaja on 8/4/15.
  */
 public class Home extends Fragment {
 
-    ToggleButton sort;
+    ToggleButton sort, version, comparison;
     ListView book_list;
     EditText book_search;
     BookDataSource book_data_source;
     BookListAdapter book_list_adapter;
     ToggleButton language;
-    Menu menu;
     SharedPreferencesLibrary spl;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
-
+        setRetainInstance(true);
     }
 
     @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -52,6 +56,8 @@ public class Home extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
         sort = (ToggleButton) rootView.findViewById(R.id.toggleButton1);
+        version = (ToggleButton) rootView.findViewById(R.id.toggleButton2);
+        comparison = (ToggleButton) rootView.findViewById(R.id.toggleButton3);
         book_list = (ListView) rootView.findViewById(R.id.listView);
         book_search = (EditText) rootView.findViewById(R.id.editText);
         language = (ToggleButton) rootView.findViewById(R.id.toggleButton);
@@ -59,20 +65,82 @@ public class Home extends Fragment {
         book_data_source = new BookDataSource(getActivity());
         ArrayList<Book> book_array = new ArrayList<Book>();
 
-        book_array = book_data_source.getIndonesianBooks();
-        book_list_adapter = new BookListAdapter(book_array, getActivity());
-        book_list.setAdapter(book_list_adapter);
-
         spl = new SharedPreferencesLibrary(getActivity());
+
+        // Populating Data
+        language.setChecked(spl.getLanguage());
+        sort.setChecked(spl.getSort());
+        comparison.setChecked(spl.getMode());
 
         if(language.isChecked())
         {
-            spl.setLanguage(true);
+            version.setTextOn("NIV");
+            version.setTextOff("ESV");
+            version.setChecked(spl.getVersion());
         }
         else
         {
-            spl.setLanguage(false);
+            version.setTextOn("Terjemahan Baru");
+            version.setTextOff("Terjemahan Baru");
+            version.setChecked(spl.getVersion());
         }
+
+        if(language.isChecked())
+        {
+            if(sort.isChecked())
+            {
+                book_array = book_data_source.getEnglishBooksAlphabetically();
+                book_list_adapter = new BookListAdapter(book_array, getActivity());
+                book_list.setAdapter(book_list_adapter);
+            }
+            else
+            {
+                book_array = book_data_source.getEnglishBooks();
+                book_list_adapter = new BookListAdapter(book_array, getActivity());
+                book_list.setAdapter(book_list_adapter);
+            }
+        }
+        else
+        {
+            if(sort.isChecked())
+            {
+                book_array = book_data_source.getIndonesianBooksAlphabetically();
+                book_list_adapter = new BookListAdapter(book_array, getActivity());
+                book_list.setAdapter(book_list_adapter);
+            }
+            else
+            {
+                book_array = book_data_source.getIndonesianBooks();
+                book_list_adapter = new BookListAdapter(book_array, getActivity());
+                book_list.setAdapter(book_list_adapter);
+            }
+        }
+
+        // LISTENERS
+        version.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                {
+                    spl.setVersion(true);
+                }
+                else
+                {
+                    spl.setVersion(false);
+                }
+            }
+        });
+
+        comparison.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    spl.setMode(true);
+                } else {
+                    spl.setMode(false);
+                }
+            }
+        });
 
         book_search.addTextChangedListener(new TextWatcher() {
 
@@ -83,33 +151,23 @@ public class Home extends Fragment {
             }
 
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count)
-            {
-                if(language.isChecked())
-                {
-                    if(sort.isChecked())
-                    {
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (language.isChecked()) {
+                    if (sort.isChecked()) {
                         book_array = book_data_source.getRelatedEnglishBooksAlphabetically(s.toString());
                         book_list_adapter = new BookListAdapter(book_array, getActivity());
                         book_list.setAdapter(book_list_adapter);
-                    }
-                    else
-                    {
+                    } else {
                         book_array = book_data_source.getRelatedEnglishBooks(s.toString());
                         book_list_adapter = new BookListAdapter(book_array, getActivity());
                         book_list.setAdapter(book_list_adapter);
                     }
-                }
-                else
-                {
-                    if(sort.isChecked())
-                    {
+                } else {
+                    if (sort.isChecked()) {
                         book_array = book_data_source.getRelatedIndonesianBooksAlphabetically(s.toString());
                         book_list_adapter = new BookListAdapter(book_array, getActivity());
                         book_list.setAdapter(book_list_adapter);
-                    }
-                    else
-                    {
+                    } else {
                         book_array = book_data_source.getRelatedIndonesianBooks(s.toString());
                         book_list_adapter = new BookListAdapter(book_array, getActivity());
                         book_list.setAdapter(book_list_adapter);
@@ -124,82 +182,72 @@ public class Home extends Fragment {
 
         language.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
-            {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 ArrayList<Book> book_array = new ArrayList<Book>();
 
-                if(book_search.getText().length() > 0)
-                {
-                    if(language.isChecked())
-                    {
+                if (book_search.getText().length() > 0) {
+                    if (language.isChecked()) {
                         // if language is english
                         spl.setLanguage(true);
-                        if(sort.isChecked())
-                        {
+                        if (sort.isChecked()) {
                             book_array = book_data_source.getRelatedEnglishBooksAlphabetically(book_search.getText().toString());
                             book_list_adapter = new BookListAdapter(book_array, getActivity());
                             book_list.setAdapter(book_list_adapter);
-                        }
-                        else
-                        {
+                        } else {
                             book_array = book_data_source.getRelatedEnglishBooks(book_search.getText().toString());
                             book_list_adapter = new BookListAdapter(book_array, getActivity());
                             book_list.setAdapter(book_list_adapter);
                         }
-                    }
-                    else
-                    {
+                        version.setTextOff("ESV");
+                        version.setTextOn("NIV");
+                        version.setChecked(version.isChecked());
+                    } else {
                         // if language is indo
                         spl.setLanguage(false);
-                        if(sort.isChecked())
-                        {
+                        if (sort.isChecked()) {
                             book_array = book_data_source.getRelatedIndonesianBooksAlphabetically(book_search.getText().toString());
                             book_list_adapter = new BookListAdapter(book_array, getActivity());
                             book_list.setAdapter(book_list_adapter);
-                        }
-                        else
-                        {
+                        } else {
                             book_array = book_data_source.getRelatedIndonesianBooks(book_search.getText().toString());
                             book_list_adapter = new BookListAdapter(book_array, getActivity());
                             book_list.setAdapter(book_list_adapter);
                         }
+                        version.setTextOff("Terjemahan Baru");
+                        version.setTextOn("Terjemahan Baru");
+                        version.setChecked(version.isChecked());
                     }
-                }
-                else
-                {
-                    if(language.isChecked())
-                    {
+                } else {
+                    if (language.isChecked()) {
                         // if language is english
                         spl.setLanguage(true);
-                        if(sort.isChecked())
-                        {
+                        if (sort.isChecked()) {
                             book_array = book_data_source.getEnglishBooksAlphabetically();
                             book_list_adapter = new BookListAdapter(book_array, getActivity());
                             book_list.setAdapter(book_list_adapter);
-                        }
-                        else
-                        {
+                        } else {
                             book_array = book_data_source.getEnglishBooks();
                             book_list_adapter = new BookListAdapter(book_array, getActivity());
                             book_list.setAdapter(book_list_adapter);
                         }
-                    }
-                    else
-                    {
+                        version.setTextOff("ESV");
+                        version.setTextOn("NIV");
+                        version.setChecked(version.isChecked());
+                    } else {
                         // if language is indo
                         spl.setLanguage(false);
-                        if(sort.isChecked())
-                        {
+                        if (sort.isChecked()) {
                             book_array = book_data_source.getIndonesianBooksAlphabetically();
                             book_list_adapter = new BookListAdapter(book_array, getActivity());
                             book_list.setAdapter(book_list_adapter);
-                        }
-                        else
-                        {
+                        } else {
                             book_array = book_data_source.getIndonesianBooks();
                             book_list_adapter = new BookListAdapter(book_array, getActivity());
                             book_list.setAdapter(book_list_adapter);
                         }
+                        version.setTextOff("Terjemahan Baru");
+                        version.setTextOn("Terjemahan Baru");
+                        version.setChecked(version.isChecked());
                     }
                 }
             }
@@ -207,70 +255,56 @@ public class Home extends Fragment {
 
         sort.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
-            {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 ArrayList<Book> book_array = new ArrayList<Book>();
 
-                if(book_search.getText().length() > 0)
-                {
-                    if(language.isChecked())
-                    {
-                        if(sort.isChecked())
-                        {
+                if (book_search.getText().length() > 0) {
+                    if (language.isChecked()) {
+                        if (sort.isChecked()) {
+                            spl.setSort(true);
                             book_array = book_data_source.getRelatedEnglishBooksAlphabetically(book_search.getText().toString());
                             book_list_adapter = new BookListAdapter(book_array, getActivity());
                             book_list.setAdapter(book_list_adapter);
-                        }
-                        else
-                        {
+                        } else {
+                            spl.setSort(false);
                             book_array = book_data_source.getRelatedEnglishBooks(book_search.getText().toString());
                             book_list_adapter = new BookListAdapter(book_array, getActivity());
                             book_list.setAdapter(book_list_adapter);
                         }
-                    }
-                    else
-                    {
-                        if(sort.isChecked())
-                        {
+                    } else {
+                        if (sort.isChecked()) {
+                            spl.setSort(true);
                             book_array = book_data_source.getRelatedIndonesianBooksAlphabetically(book_search.getText().toString());
                             book_list_adapter = new BookListAdapter(book_array, getActivity());
                             book_list.setAdapter(book_list_adapter);
-                        }
-                        else
-                        {
+                        } else {
+                            spl.setSort(false);
                             book_array = book_data_source.getRelatedIndonesianBooks(book_search.getText().toString());
                             book_list_adapter = new BookListAdapter(book_array, getActivity());
                             book_list.setAdapter(book_list_adapter);
                         }
                     }
-                }
-                else
-                {
-                    if(language.isChecked())
-                    {
-                        if(sort.isChecked())
-                        {
+                } else {
+                    if (language.isChecked()) {
+                        if (sort.isChecked()) {
+                            spl.setSort(true);
                             book_array = book_data_source.getEnglishBooksAlphabetically();
                             book_list_adapter = new BookListAdapter(book_array, getActivity());
                             book_list.setAdapter(book_list_adapter);
-                        }
-                        else
-                        {
+                        } else {
+                            spl.setSort(false);
                             book_array = book_data_source.getEnglishBooks();
                             book_list_adapter = new BookListAdapter(book_array, getActivity());
                             book_list.setAdapter(book_list_adapter);
                         }
-                    }
-                    else
-                    {
-                        if(sort.isChecked())
-                        {
+                    } else {
+                        if (sort.isChecked()) {
+                            spl.setSort(true);
                             book_array = book_data_source.getIndonesianBooksAlphabetically();
                             book_list_adapter = new BookListAdapter(book_array, getActivity());
                             book_list.setAdapter(book_list_adapter);
-                        }
-                        else
-                        {
+                        } else {
+                            spl.setSort(false);
                             book_array = book_data_source.getIndonesianBooks();
                             book_list_adapter = new BookListAdapter(book_array, getActivity());
                             book_list.setAdapter(book_list_adapter);
@@ -309,12 +343,21 @@ public class Home extends Fragment {
 
                         spl.setBibleIndex(new Chapter(book_name, Integer.parseInt(button.getText().toString())));
 
-                        Fragment fragment = new Bible();
-                        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                        fragmentTransaction.replace(R.id.container, fragment);
-                        fragmentTransaction.addToBackStack(null);
-                        fragmentTransaction.commit();
+                        if (comparison.isChecked()) {
+                            spl.setState(2);
+                            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                            fragmentTransaction.replace(R.id.container, new Comparison());
+                            fragmentTransaction.addToBackStack(null);
+                            fragmentTransaction.commit();
+                        } else {
+                            spl.setState(1);
+                            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                            fragmentTransaction.replace(R.id.container, new Bible());
+                            fragmentTransaction.addToBackStack(null);
+                            fragmentTransaction.commit();
+                        }
 
                         dialog.dismiss();
                     }
